d8d9d6d5b2dbb65d2282e4e7d97796b496ecb9fc
Admin <artem-ruslanovich@inbox.ru>
2024-05-12 16:36:24 +0300

    add anim

---
+++ myproject/myapp/static/js/header.js
@@ -0,0 +1,18 @@
+var header = document.getElementById("header");
+
+// Получаем позицию элемента header относительно верха документа
+var sticky = header.offsetTop;
+
+// Функция, которая будет вызываться при прокрутке страницы
+function stickyHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("fixed");
+    } else {
+        header.classList.remove("fixed");
+    }
+}
+
+// Добавляем обработчик прокрутки страницы
+window.onscroll = function() {
+    stickyHeader();
+};
