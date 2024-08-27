c8cd9814009cc49103d7f2c4220bcbd63ab5cbb9
Admin <artiomzavatski@gmail.com>
2024-08-27 23:13:30 +0300

    edits

--- myproject/myapp/static/js/menu.js
+++ myproject/myapp/static/js/menu.js
@@ -1,18 +1,19 @@
-// Обработчик для кнопки "Меню"
-document.getElementById("menu-btn").addEventListener("click", function(event) {
-    event.preventDefault();
-    document.getElementById("menu-photo").style.display = "flex";
-    document.getElementById("overlay").style.display = "block";
-});
 
-// Обработчик для затемненного фона
-document.getElementById("overlay").addEventListener("click", function() {
-    document.getElementById("menu-photo").style.display = "none";
-    this.style.display = "none"; // Используем "this" для обращения к элементу overlay
-});
+document.addEventListener("DOMContentLoaded", function() {
+    // Ваш код здесь
+    document.getElementById("menu-btn").addEventListener("click", function(event) {
+        event.preventDefault();
+        document.getElementById("menu-photo").style.display = "flex";
+        document.getElementById("overlay").style.display = "block";
+    });
 
-// Дополнительный обработчик для закрытия при клике на изображение
-document.getElementById("menu-photo").addEventListener("click", function() {
-    this.style.display = "none";
-    document.getElementById("overlay").style.display = "none";
+    document.getElementById("overlay").addEventListener("click", function() {
+        document.getElementById("menu-photo").style.display = "none";
+        this.style.display = "none";
+    });
+
+    document.getElementById("menu-photo").addEventListener("click", function() {
+        this.style.display = "none";
+        document.getElementById("overlay").style.display = "none";
+    });
 });
