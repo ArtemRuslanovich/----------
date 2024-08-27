bb8e37606cf71b0158fb8d6340ba5b062bc0bd08
Admin <artiomzavatski@gmail.com>
2024-08-27 22:40:51 +0300

    edit menu

--- myproject/myapp/static/js/menu.js
+++ myproject/myapp/static/js/menu.js
@@ -1,9 +1,10 @@
 document.getElementById("menu-btn").addEventListener("click", function(event) {
     event.preventDefault();
-    document.getElementById("menu-photo").style.display = "block";
+    document.getElementById("menu-photo").style.display = "flex"; // Используем "flex" вместо "block"
     document.getElementById("overlay").style.display = "block";
 });
 
+// Обработчик для затемненного фона
 document.getElementById("overlay").addEventListener("click", function() {
     document.getElementById("menu-photo").style.display = "none";
     document.getElementById("overlay").style.display = "none";
