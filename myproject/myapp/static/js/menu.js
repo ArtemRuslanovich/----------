7008f9ec2f3392176646f5f879cba48ca2922d87
Admin <artiomzavatski@gmail.com>
2024-08-27 22:57:08 +0300

    fix menu

--- myproject/myapp/static/js/menu.js
+++ myproject/myapp/static/js/menu.js
@@ -1,11 +1,18 @@
+// Обработчик для кнопки "Меню"
 document.getElementById("menu-btn").addEventListener("click", function(event) {
     event.preventDefault();
-    document.getElementById("menu-photo").style.display = "flex"; // Используем "flex" вместо "block"
+    document.getElementById("menu-photo").style.display = "flex";
     document.getElementById("overlay").style.display = "block";
 });
 
 // Обработчик для затемненного фона
 document.getElementById("overlay").addEventListener("click", function() {
     document.getElementById("menu-photo").style.display = "none";
+    this.style.display = "none"; // Используем "this" для обращения к элементу overlay
+});
+
+// Дополнительный обработчик для закрытия при клике на изображение
+document.getElementById("menu-photo").addEventListener("click", function() {
+    this.style.display = "none";
     document.getElementById("overlay").style.display = "none";
 });
